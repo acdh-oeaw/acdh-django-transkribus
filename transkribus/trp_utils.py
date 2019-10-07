ed3b5ec07bcf1c214ac9cd9af6ffccb41fe3f7c4
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-10-07 12:53:35 +0200

    added some docstrings to main function

--- transkribus/trp_utils.py
+++ transkribus/trp_utils.py
@@ -11,29 +11,40 @@ nsmap = {
 
 try:
     user = settings.TRANSKRIBUS['user']
-except AttributeError:
+except (AttributeError, KeyError) as e:
     print("no TRANSKRIBUS user set in the project's settings file")
+    print(e)
     user = "user"
 
 try:
     pw = settings.TRANSKRIBUS['pw']
-except AttributeError:
+except (AttributeError, KeyError) as e:
     print("no TRANSKRIBUS pw set in the project's settings file")
+    print(e)
     pw = "pw"
 
 try:
     col_id = settings.TRANSKRIBUS['col_id']
-except AttributeError:
+except (AttributeError, KeyError) as e:
     print("no TRANSKRIBUS col_id set in the project's settings file")
+    print(e)
     col_id = None
 
 try:
     base_url = settings.TRANSKRIBUS['base_url']
-except AttributeError:
+except (AttributeError, KeyError) as e:
+    print("no TRANSKRIBUS base_url set in the project's settings file")
+    print(e)
     base_url = "https://transkribus.eu/TrpServer/rest"
 
 
 def trp_login(user, pw, base_url=base_url):
+    """ log in function
+        :param user: Your TRANSKRIBUS user name, e.g. my.mail@whatever.com
+        :param pw: Your TRANSKRIBUS password
+        :param base_url: The base URL of the TRANSKRIBUS API
+        :return: The Session ID in case of a successful log in attempt
+    """
     url = "{}/auth/login".format(base_url)
     payload = "user={}&pw={}".format(user, pw)
     headers = {
@@ -48,6 +59,14 @@ def trp_login(user, pw, base_url=base_url):
 
 
 def trp_ft_search(base_url=base_url, user=user, pw=pw, **kwargs):
+    """ Helper function to interact with TRANSKRIBUS fulltext search endpoint
+        :param user: Your TRANSKRIBUS user name, e.g. my.mail@whatever.com
+        :param pw: Your TRANSKRIBUS password
+        :param base_url: The base URL of the TRANSKRIBUS API
+        :param kwargs: kwargs will be forwarded to TRANSKRIBUS API endpoint e.g. 'query' holds the\
+        search string
+        :return: The default TRANSKRIBUS response as JSON
+    """
     url = f"{base_url}/search/fulltext"
     if kwargs:
         querystring = kwargs
@@ -67,6 +86,15 @@ def trp_ft_search(base_url=base_url, user=user, pw=pw, **kwargs):
 
 
 def trp_get_fulldoc_md(doc_id, base_url=base_url, col_id=col_id, page_id="1", user=user, pw=pw):
+    """ Helper function to interact with TRANSKRIBUS document endpoint
+        :param user: Your TRANSKRIBUS user name, e.g. my.mail@whatever.com
+        :param pw: Your TRANSKRIBUS password
+        :param base_url: The base URL of the TRANSKRIBUS API
+        :col_id: The ID of a TRANSKRIBUS Collection
+        :doc_id: The ID of TRANSKRIBUS Document
+        :page_id: The page number of the Document
+        :return: A dict with basic metadata of a transkribus Document
+    """
     url = f"{base_url}/collections/{col_id}/{doc_id}/{page_id}"
     session_id = trp_login(user, pw, base_url=base_url)
     headers = {
@@ -94,7 +122,11 @@ def trp_get_fulldoc_md(doc_id, base_url=base_url, col_id=col_id, page_id="1", us
 
 
 def get_transcript(fulldoc_md):
-    nsmap = nsmap = {
+    """ Helper function to fetch the (latest) fulltext of a TRANSKRIBUS page
+        :param fulldoc_md: A dict returned by trp_login.trp_get_fulldoc_md
+        :return: The fulldoc_md dict with additional keys 'page_xml' and 'transcript'
+    """
+    nsmap = {
         "page": "http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15"
     }
     md = fulldoc_md
@@ -115,6 +147,13 @@ def get_transcript(fulldoc_md):
 
 
 def list_documents(base_url=base_url, col_id=col_id, user=user, pw=pw):
+    """ Helper function to interact with TRANSKRIBUS collection endpoint to list all documents
+        :param user: Your TRANSKRIBUS user name, e.g. my.mail@whatever.com
+        :param pw: Your TRANSKRIBUS password
+        :param base_url: The base URL of the TRANSKRIBUS API
+        :col_id: The ID of a TRANSKRIBUS Collection
+        :return: A dict with the default TRANSKRIBUS API return
+    """
     url = f"{base_url}/collections/{col_id}/list"
     session_id = trp_login(user, pw, base_url=base_url)
     headers = {
